1aafd04c66e344e4b80a28fa4b191cb5a1f8c9f2
Andrew Fuller <qartis@gmail.com>
2011-11-07 16:12:29 -0800

    make animations smoother on windows

--- gui.h
+++ gui.h
@@ -5,8 +5,8 @@ typedef enum drop_type (*canclick_func_t)(void *obj, int x, int y);
 typedef void (*resetgame_func_t)(void *obj);
 typedef void (gui_t::*draw_func_t)(struct anim_t *anim, float elapsed);
 
-#define ANIM_LEN 0.7f
-#define ANIM_FPS 1.0f/60.0f
+#define ANIM_LEN 0.6f
+#define ANIM_FPS 1.0f/90.0f
 
 enum anim_type {
     ANIM_DROP,
